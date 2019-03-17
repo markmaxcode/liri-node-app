5faf5752644e96e57ccc2257724d02b9b195e506
markmaxcodee <markferraro@Marks-MBP.home>
2019-03-17 19:02:40 -0400

    bands segment

--- liri.js
+++ liri.js
@@ -1,10 +1,20 @@
+var config = require("dotenv").config();
 var axios = require("axios");
 console.log("hello");
-var artist = "sting";
-
+console.log(process.env);
+console.log(process.argv);
+var commandOption = (process.argv[2]);
+var requestOption = (process.argv[3]);
+if (commandOption==="concert-this"){
+    getConcertInfo(requestOption);
+} else {
+    console.log("invalid");
+}
+function getConcertInfo(artist){ 
 axios.get("https://rest.bandsintown.com/artists/" + artist + "/events?app_id=codingbootcamp")
   .then(function (response) {
-    console.log(response.data);
+    //console.log(response.data);
+    console.log("data came backe");
     for (i=0; i<response.data.length; i++){
         console.log("city:" + response.data[i].venue.city);
         console.log("Venue:" + response.data[i].venue.name);
@@ -15,3 +25,4 @@ axios.get("https://rest.bandsintown.com/artists/" + artist + "/events?app_id=cod
   .catch(function (error) {
     console.log(error);
   });
+}
